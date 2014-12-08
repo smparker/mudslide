a8fefb5baf4535fa654e4c0a0def89d239d327eb
Shane Parker <shane.parker@u.northwestern.edu>
2014-12-08 14:22:50 -0800

    lots of bug fixes, it now runs albeit slowly

--- fssh.py
+++ fssh.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import scipy.integrate
 import math as m
 
 ## Tunneling through a single barrier model used in Tully's 1990 JCP
@@ -56,25 +57,25 @@ class ElectronicStates:
 
     ## returns dimension of Hamiltonian
     def dim(self):
-        return V.shape[0]
+        return self.V.shape[0]
 
     ## returns \f$-\langle \phi_{\mbox{state}} | \nabla H | \phi_{\mbox{state}} \rangle\f$ of Hamiltonian
     # @param state state along which to get
     def compute_force(self, state):
-        state_vec = coeff[:,state]
-        force = - (np.dot(state_vec.T, np.dot(dV, state_vec)))
+        state_vec = self.coeff[:,state]
+        force = - (np.dot(state_vec.T, np.dot(self.dV, state_vec)))
         return force
 
     def compute_derivative_coupling(self, bra_state, ket_state):
         out = 0.0
         if (bra_state != ket_state):
-            out = np.dot(coeff[:,bra_state].T, np.dot(dV, coeff[:,ket_state]))
-            out /= energies[bra_state] - energies[ket_state]
+            out = np.dot(self.coeff[:,bra_state].T, np.dot(self.dV, self.coeff[:,ket_state]))
+            out /= self.energies[bra_state] - self.energies[ket_state]
         return out
 
     def compute_NAC_matrix(self, velocity):
-        out = np.zeros([dim(), dim()], dtype=complex64)
-        out[0,1] = compute_derivative_coupling(0, 1)
+        out = np.zeros([self.dim(), self.dim()], dtype=np.complex64)
+        out[0,1] = self.compute_derivative_coupling(0, 1)
         out[1,0] = - out[0,1]
         out *= -1j * velocity
         return out
@@ -106,19 +107,20 @@ class Trajectory:
             raise Exception("Unrecognized initial state option")
 
         # fixed initial parameters
-        self.time = options["time"]
         self.step = 0
 
         # read out of options
         self.dt = options["dt"]
-        self.nsteps = options["total_time"] / self.dt
+        self.nsteps = int(options["total_time"] / self.dt)
 
-    def kinetic_energy(self, component = self.velocity):
+    def kinetic_energy(self, component = None):
+        if component == None:
+            component = self.velocity
         return 0.5 * self.mass * np.dot(component, component)
 
     def mode_kinetic_energy(self, direction):
         component = np.dot(direction, self.velocity) / np.dot(direction, direction) * direction
-        return kinetic_energy(component)
+        return kinetic_energy(self, component)
 
     ## Rescales velocity in the specified direction and amount
     # @param direction array specifying the direction of the velocity to rescale
@@ -137,25 +139,25 @@ class Trajectory:
     def step_forward(self):
         self.position += self.velocity * self.dt + 0.5 * self.acceleration * self.dt * self.dt
 
-    def compute_force(self, phi, Vgrad):
-
     ## Propagates \f$\rho(t)\f$ to \f$\rho(t + dt)\f$
     # @param elec_states_0 ElectronicStates at \f$t\f$
     # @param elec_states_1 ElectronicStates at \f$t + dt\f$
     def propagate_rho(self, elec_states_0, elec_states_1):
         def drho(time, y):
+            ymat = np.reshape(y, [2, 2])
             H = 0.5 * (elec_states_0.V + elec_states_1.V)
             D = 0.5 * (elec_states_0.compute_NAC_matrix(self.velocity)
                         + elec_states_1.compute_NAC_matrix(self.velocity))
-            tmp = np.dot(H + D, y)
+            tmp = np.dot(H + D, ymat)
             dro = -1j * (tmp - np.conj(tmp.T))
-            return dro
+            return np.reshape(dro, [4])
 
-        integrator = scipy.integrate.ode(drho).set_integrator('zvode', method='bdf', with_jacobian=False)
-        integrator.set_initial_value(self.rho, self.time)
+        rhovec = np.reshape(self.rho, [4])
+        integrator = scipy.integrate.complex_ode(drho).set_integrator('vode', method='bdf', with_jacobian=False)
+        integrator.set_initial_value(rhovec, self.time)
         integrator.integrate(self.time + self.dt)
         self.rho = integrator.y
-        if !integrator.successful():
+        if not integrator.successful():
             exit("Propagation of the electronic wavefunction failed!")
 
     def surface_hopping(self, elec_states):
@@ -169,28 +171,28 @@ class Trajectory:
             target_state = 1-self.state
             new_potential, old_potential = elec_states.energies[target_state], elec_states.energies[self.state]
             delV = new_potential - old_potential
-            component_kinetic = mode_kinetic_energy(np.ones([1])
+            component_kinetic = mode_kinetic_energy(np.ones([1]))
             if delV <= component_kinetic:
                 self.state = target_state
                 rescale_component(np.ones([1]), delV)
 
     def simulate(self):
-        electronics = ElectronicState(model.V(self.position), model.Vgrad(self.position))
+        electronics = ElectronicStates(model.V(self.position), model.Vgrad(self.position))
         # start by taking half step in velocity
         initial_acc = electronics.compute_force(self.state) / self.mass
         self.velocity += 0.5 * initial_acc * self.dt
 
         # propagation
-        for step in range(nsteps):
+        for step in range(self.nsteps):
             # first update nuclear coordinates
             self.position += self.velocity * self.dt
             # calculate electronics at new position
-            new_electronics = ElectronicState(model.V(self.position), model.Vgrad(self.position))
+            new_electronics = ElectronicStates(model.V(self.position), model.Vgrad(self.position))
             acceleration = new_electronics.compute_force(self.state) / self.mass
             self.velocity += acceleration * self.dt
 
             # now propagate the electronic wavefunction to the new time
-            propagate_rho(electronics, new_electronics)
+            self.propagate_rho(electronics, new_electronics)
 
     def outcome(self):
         # first bit is left (0) or right (1), second bit is electronic state
@@ -210,8 +212,8 @@ class FSSH:
         # system parameters
         self.options["initial_state"] = inp.get("initial_state", "ground")
         self.options["position"]      = inp.get("position", -10.0)
-        self.options["velocity"]      = inp.get("velocity", 2.0)
         self.options["mass"]          = inp.get("mass", 2000.0)
+        self.options["velocity"]      = inp.get("momentum", 2.0) / self.options["mass"]
 
         # time parameters
         self.options["initial_time"]  = inp.get("initial_time", 0.0)
@@ -223,41 +225,28 @@ class FSSH:
 
     def compute(self):
         # for now, define four possible outcomes of the simulation
-        outcomes = (0.0, 0.0, 0.0, 0.0)
+        outcomes = [0.0, 0.0, 0.0, 0.0]
         nsamples = int(self.options["samples"])
         for it in range(nsamples):
-            traj = Trajectory(self.model, options)
+            traj = Trajectory(self.model, self.options)
             traj.simulate()
             outcomes[traj.outcome()] += 1
         outcomes /= float(nsamples)
         return outcomes
 
-model = TullyModel()
+if __name__ == "__main__":
+    model = TullyModel()
 
-# compute barrier
-#diss_ham = model.V(-10000)
-#diss_energy = np.linalg.eigvalsh(diss_ham)[0]
-#int_ham = model.V(0)
-#int_energy = np.linalg.eigvalsh(int_ham)[0]
-#
-## print surfaces for debugging
-#nr = np.linspace(-10, 10, 200 + 1)
-#
-#traj = Trajectory(-13, 0.10000, 1, np.zeros([2,2]), {"dt" : 0.1})
-#nsteps = 5000
-#for i in range(nsteps):
-#    ham = model.V(traj.position)
-#    dham = model.Vgrad(traj.position)
-#    energies, adiabats = np.linalg.eigh(ham)
-#    traj.nuclear_step(adiabats[:,0], dham)
-#    print "%6d %12.6f" % (i, traj.position)
-#
-#
-#for r in nr:
-#    ham = model.V(r)
-#    dham = model.Vgrad(r)
-#    energies, adiabats = np.linalg.eigh(ham)
-#    force = traj.nuclear_step(adiabats[:,0], dham)
-#    d = traj.derivative_coupling(adiabats[:,0], energies[0], adiabats[:,1], energies[1], dham)
-#    #print "%12.6f %12.6f %12.6f %12.6f %12.6f %12.6f %12.6f" % (r, ham[0,0], ham[1,1], energies[0], energies[1], gradient, d)
-#    print "%12.6f %12.6f %12.6f %12.6f %12.6f %12.6f" % (r, ham[0,0], energies[0], dham[0,0], force, d)
+    nk = int(100)
+    max_k = float(30.0)
+    min_k = max_k / nk
+    kpoints = np.linspace(min_k+5, max_k, nk)
+    for k in kpoints:
+        fssh = FSSH(model, momentum = k,
+                           position = -5.0,
+                           mass = 2000.0,
+                           total_time = 10.0 / (k / 2000.0),
+                           dt = 10,
+                           samples = 100)
+        results = fssh.compute()
+        print "%12.6f %12.6f %12.6f %12.6f %12.6f" % (k, results[0], results[1], results[2], results[3])
