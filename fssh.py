f34641f4f10db045f95c223b053363424afac257
Shane Parker <shane.parker@u.northwestern.edu>
2014-12-08 11:58:11 -0800

    all the pieces are there, but not yet tested.

--- fssh.py
+++ fssh.py
@@ -36,46 +36,147 @@ class TullyModel:
 #class FSSH:
 #    
 
+class ElectronicStates:
+    """ Wrapper around all the information computed for a set of electronics
+        states at a given position: V, dV, eigenvectors, eigenvalues """
+    V = np.zeros([2, 2])
+    dV = np.zeros([2, 2])
+    coeff = np.zeros([2,2])
+    energies = np.zeros([2])
+
+    def __init__(self, V, Vgrad):
+        self.V = V
+        self.dV = Vgrad
+        self.energies, self.coeff = np.linalg.eigh(V)
+
+    def dim(self):
+        return V.shape[0]
+
+    def compute_force(self, state):
+        state_vec = coeff[:,state]
+        force = - (np.dot(state_vec.T, np.dot(dV, state_vec)))
+        return force
+
+    def compute_derivative_coupling(self, bra_state, ket_state):
+        out = 0.0
+        if (bra_state != ket_state):
+            out = np.dot(coeff[:,bra_state].T, np.dot(dV, coeff[:,ket_state]))
+            out /= energies[bra_state] - energies[ket_state]
+        return out
+
+    def compute_NAC_matrix(self, velocity):
+        out = np.zeros([dim(), dim()], dtype=complex64)
+        out[0,1] = compute_derivative_coupling(0, 1)
+        out[1,0] = - out[0,1]
+        out *= -1j * velocity
+        return out
+
 class Trajectory:
-    previous_position = 0.0
+    model = TullyModel()
     position = 0.0
     velocity = 0.0
+    acceleration = 0.0
     time = 0.0
     mass = 1.0
-    rho = np.zeros([2,2])
+    rho = np.zeros([2,2], dtype=np.complex64, order='F')
     step = 0
     state = 0
     dt = 0.1
 
-    def __init__(self, p, v, m, rho, options):
+    def __init__(self, mod, p, v, m, initial_rho, initial_state, options):
         # input explicitly
+        self.model = mod
         self.position = p
         self.velocity = v
         self.mass = m
-        self.rho = rho
+        self.rho = initial_rho
+        self.state = initial_state
 
         # fixed initial parameters
         self.time = 0.0
-        self.step
+        self.step = 0
 
         # read out of options
         self.dt = options["dt"]
+        self.nsteps = options["nsteps"]
 
-    def nuclear_step(self, phi, Vgrad):
-        force = - (np.dot(phi.T, np.dot(Vgrad, phi)))
-        x = self.position
-        xlast = self.previous_position
-        xnew = 0.0
-        if self.step == 0:
-            xnew = x + self.velocity * self.dt + 0.5 * force * self.dt * self.dt
-        else:
-            xnew = 2.0 * x - xlast + force * self.dt * self.dt
-        self.step += 1
-        self.position, self.previous_position = xnew, self.position
+    def kinetic_energy(self, component = self.velocity):
+        return 0.5 * self.mass * np.dot(component, component)
 
-    def derivative_coupling(self, phi_i, E_i, phi_j, E_j, Vgrad):
-        matel = np.dot(phi_i.T, np.dot(Vgrad, phi_j))
-        return matel / (E_i - E_j)
+    def mode_kinetic_energy(self, direction):
+        component = np.dot(direction, self.velocity) / np.dot(direction, direction) * direction
+        return kinetic_energy(component)
+
+    ## Rescales velocity in the specified direction and amount
+    # @param direction array specifying the direction of the velocity to rescale
+    # @param reduction scalar specifying how much kinetic energy should be damped
+    def rescale_component(self, direction, reduction):
+        # normalize
+        direction /= np.dot(direction, direction)
+        Md = self.mass * direction
+        a = np.dot(Md, Md)
+        b = 2.0 * self.mass * np.dot(self.velocity, Md)
+        c = reduction
+        roots = np.roots([a, b, c])
+        scal = min(roots, key=lambda x: abs(x))
+        self.velocity += scale * direction
+
+    def step_forward(self):
+        self.position += self.velocity * self.dt + 0.5 * self.acceleration * self.dt * self.dt
+
+    def compute_force(self, phi, Vgrad):
+
+    # the best way to do this would probably be linearly interpolating between the two electronics
+    # I'll use the average for now
+    def propagate_rho(self, elec_states_0, elec_states_1):
+        def drho(time, y):
+            H = 0.5 * (elec_states_0.V + elec_states_1.V)
+            D = 0.5 * (elec_states_0.compute_NAC_matrix(self.velocity)
+                        + elec_states_1.compute_NAC_matrix(self.velocity))
+            tmp = np.dot(H + D, y)
+            dro = -1j * (tmp - np.conj(tmp.T))
+            return dro
+
+        integrator = scipy.integrate.ode(drho).set_integrator('zvode', method='bdf', with_jacobian=False)
+        integrator.set_initial_value(self.rho, self.time)
+        integrator.integrate(self.time + self.dt)
+        self.rho = integrator.y
+        if !integrator.successful():
+            exit("Propagation of the electronic wavefunction failed!")
+
+    def surface_hopping(self, elec_states):
+        d01 = elec_states.compute_derivative_coupling(0, 1)
+        b01 = -2.0 * np.real(self.rho[0,1]) * self.velocity * d01
+        # probability of hopping out of current state
+        P = self.dt * b01 / self.rho[self.state, self.state]
+        zeta = np.random.uniform()
+        if zeta < P: # do switch
+            # beware, this will only work for a two-state model
+            target_state = 1-self.state
+            new_potential, old_potential = elec_states.energies[target_state], elec_states.energies[self.state]
+            delV = new_potential - old_potential
+            component_kinetic = mode_kinetic_energy(np.ones([1])
+            if delV <= component_kinetic:
+                self.state = target_state
+                rescale_component(np.ones([1]), delV)
+
+    def simulate(self):
+        electronics = ElectronicState(model.V(self.position), model.Vgrad(self.position))
+        # start by taking half step in velocity
+        initial_acc = electronics.compute_force(self.state) / self.mass
+        self.velocity += 0.5 * initial_acc * self.dt
+
+        # propagation
+        for step in range(nsteps):
+            # first update nuclear coordinates
+            self.position += self.velocity * self.dt
+            # calculate electronics at new position
+            new_electronics = ElectronicState(model.V(self.position), model.Vgrad(self.position))
+            acceleration = new_electronics.compute_force(self.state) / self.mass
+            self.velocity += acceleration * self.dt
+
+            # now propagate the electronic wavefunction to the new time
+            propagate_rho(electronics, new_electronics)
 
 model = TullyModel()
 
