8d6e420b1fb6270a454923f038d0989ad4ff1aa0
Shane Parker <shane.parker@u.northwestern.edu>
2014-12-08 13:37:18 -0800

    write FSSH class and include input values

--- fssh.py
+++ fssh.py
@@ -3,6 +3,7 @@
 import numpy as np
 import math as m
 
+## Tunneling through a single barrier model used in Tully's 1990 JCP
 class TullyModel:
     # parameters in Tully's model
     A = 0.0
@@ -33,25 +34,32 @@ class TullyModel:
                          [v12, v22] ])
         return out
 
-#class FSSH:
-#    
-
+## Wrapper around all the information computed for a set of electronics
+#  states at a given position: V, dV, eigenvectors, eigenvalues
 class ElectronicStates:
-    """ Wrapper around all the information computed for a set of electronics
-        states at a given position: V, dV, eigenvectors, eigenvalues """
+    ## \f$H\f$
     V = np.zeros([2, 2])
+    ## \f$\nabla H\f$
     dV = np.zeros([2, 2])
+    ## eigenvectors of \f$H\f$
     coeff = np.zeros([2,2])
+    ## eigenvalues of \f$H\f$
     energies = np.zeros([2])
 
+    ## Constructor
+    # @param V Hamiltonian/potential
+    # @param Vgrad Gradient of Hamiltonian/potential
     def __init__(self, V, Vgrad):
         self.V = V
         self.dV = Vgrad
         self.energies, self.coeff = np.linalg.eigh(V)
 
+    ## returns dimension of Hamiltonian
     def dim(self):
         return V.shape[0]
 
+    ## returns \f$-\langle \phi_{\mbox{state}} | \nabla H | \phi_{\mbox{state}} \rangle\f$ of Hamiltonian
+    # @param state state along which to get
     def compute_force(self, state):
         state_vec = coeff[:,state]
         force = - (np.dot(state_vec.T, np.dot(dV, state_vec)))
@@ -71,6 +79,7 @@ class ElectronicStates:
         out *= -1j * velocity
         return out
 
+## Class to propagate a single FSSH Trajectory
 class Trajectory:
     model = TullyModel()
     position = 0.0
@@ -83,22 +92,26 @@ class Trajectory:
     state = 0
     dt = 0.1
 
-    def __init__(self, mod, p, v, m, initial_rho, initial_state, options):
+    def __init__(self, model, options):
         # input explicitly
-        self.model = mod
-        self.position = p
-        self.velocity = v
-        self.mass = m
-        self.rho = initial_rho
-        self.state = initial_state
+        self.model = model
+        self.position = options["position"]
+        self.velocity = options["velocity"]
+        self.mass = options["mass"]
+        if options["initial_state"] == "ground":
+            self.rho = np.zeros([2,2], dtype = np.complex64)
+            self.rho[0,0] = 1.0
+            self.state = 0
+        else:
+            raise Exception("Unrecognized initial state option")
 
         # fixed initial parameters
-        self.time = 0.0
+        self.time = options["time"]
         self.step = 0
 
         # read out of options
         self.dt = options["dt"]
-        self.nsteps = options["nsteps"]
+        self.nsteps = options["total_time"] / self.dt
 
     def kinetic_energy(self, component = self.velocity):
         return 0.5 * self.mass * np.dot(component, component)
@@ -179,27 +192,66 @@ class Trajectory:
             # now propagate the electronic wavefunction to the new time
             propagate_rho(electronics, new_electronics)
 
+    def outcome(self):
+        # first bit is left (0) or right (1), second bit is electronic state
+        lr = 0 if self.position < 0.0 else 1
+        return 2*self.state + lr
+
+## Class to manage many FSSH trajectories
+class FSSH:
+    ## model to be used for the simulations
+    model = TullyModel()
+    ## input options
+    options = {}
+
+    def __init__(self, model, **inp):
+        self.model = model
+
+        # system parameters
+        self.options["initial_state"] = inp.get("initial_state", "ground")
+        self.options["position"]      = inp.get("position", -10.0)
+        self.options["velocity"]      = inp.get("velocity", 2.0)
+        self.options["mass"]          = inp.get("mass", 2000.0)
+
+        # time parameters
+        self.options["initial_time"]  = inp.get("initial_time", 0.0)
+        self.options["dt"]            = inp.get("dt", 0.1)
+        self.options["total_time"]    = inp.get("total_time", abs(self.options["position"] / self.options["velocity"]))
+
+        # statistical parameters
+        self.options["samples"]       = inp.get("samples", 2000)
+
+    def compute(self):
+        # for now, define four possible outcomes of the simulation
+        outcomes = (0.0, 0.0, 0.0, 0.0)
+        nsamples = int(self.options["samples"])
+        for it in range(nsamples):
+            traj = Trajectory(self.model, options)
+            traj.simulate()
+            outcomes[traj.outcome()] += 1
+        outcomes /= float(nsamples)
+        return outcomes
+
 model = TullyModel()
 
 # compute barrier
-
-diss_ham = model.V(-10000)
-diss_energy = np.linalg.eigvalsh(diss_ham)[0]
-int_ham = model.V(0)
-int_energy = np.linalg.eigvalsh(int_ham)[0]
-
-# print surfaces for debugging
-nr = np.linspace(-10, 10, 200 + 1)
-
-traj = Trajectory(-13, 0.10000, 1, np.zeros([2,2]), {"dt" : 0.1})
-nsteps = 5000
-for i in range(nsteps):
-    ham = model.V(traj.position)
-    dham = model.Vgrad(traj.position)
-    energies, adiabats = np.linalg.eigh(ham)
-    traj.nuclear_step(adiabats[:,0], dham)
-    print "%6d %12.6f" % (i, traj.position)
-
+#diss_ham = model.V(-10000)
+#diss_energy = np.linalg.eigvalsh(diss_ham)[0]
+#int_ham = model.V(0)
+#int_energy = np.linalg.eigvalsh(int_ham)[0]
+#
+## print surfaces for debugging
+#nr = np.linspace(-10, 10, 200 + 1)
+#
+#traj = Trajectory(-13, 0.10000, 1, np.zeros([2,2]), {"dt" : 0.1})
+#nsteps = 5000
+#for i in range(nsteps):
+#    ham = model.V(traj.position)
+#    dham = model.Vgrad(traj.position)
+#    energies, adiabats = np.linalg.eigh(ham)
+#    traj.nuclear_step(adiabats[:,0], dham)
+#    print "%6d %12.6f" % (i, traj.position)
+#
 #
 #for r in nr:
 #    ham = model.V(r)
