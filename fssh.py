c915c52901b39f1fb5f65d4a18043690d3938aa4
Shane Parker <smparker@uci.edu>
2016-09-29 16:28:12 -0700

    adds some ability to hack the output

--- fssh.py
+++ fssh.py
@@ -539,7 +539,8 @@ if __name__ == "__main__":
     parser.add_argument('-T', '--nt', default=50000, type=int, help="max number of steps (%(default)s)")
     parser.add_argument('-x', '--position', default=-10.0, type=float, help="starting position (%(default)s)")
     parser.add_argument('-b', '--bounds', default=5.0, type=float, help="bounding box to end simulation (%(default)s)")
-    parser.add_argument('-o', '--output', default="averaged", type=str, choices=('averaged', 'single', 'pickle'), help="what to produce as output (%(default)s)")
+    parser.add_argument('-o', '--output', default="averaged", type=str, choices=('averaged', 'single', 'pickle', 'hack'), help="what to produce as output (%(default)s)")
+    parser.add_argument('-O', '--outfile', default="sh.pickle", type=str, help="name of pickled file to produce (%(default)s)")
     parser.add_argument('-z', '--seed', default=None, type=int, help="random seed (current date)")
     parser.add_argument('--published', dest="published", action="store_true", help="override ranges to use those found in relevant papers (%(default)s)")
 
@@ -579,7 +580,7 @@ if __name__ == "__main__":
     CheckEnd.box_bounds = args.bounds
     CheckEnd.nsteps = args.nt
 
-    all_results = {}
+    all_results = []
 
     for k in kpoints:
         if args.ksampling == "none":
@@ -604,14 +605,17 @@ if __name__ == "__main__":
         results = fssh.compute()
         outcomes = results.outcomes
 
-        if (args.output == "averaged"):
-            print("%12.6f %s" % (k, " ".join(["%12.6f" % x for x in np.nditer(outcomes)])))
-        elif (args.output == "single"):
+        if (args.output == "single"):
             for i in results.traces[0]:
                 print("%12.6f %12.6f %12.6f %6d" % (i.time, i.position, i.momentum, i.activestate))
-        elif (args.output == "pickle"): # save results for later processing
-            all_results[k] = results
+        elif (args.output == "averaged" or args.output == "pickle"):
+            print("%12.6f %s" % (k, " ".join(["%12.6f" % x for x in np.nditer(outcomes)])))
+            if (args.output == "pickle"): # save results for later processing
+                all_results.append((k, results))
+        elif (args.output == "hack"):
+            print("Put your very own output functions here.")
         else:
             print("Not printing results. This is probably not what you wanted!")
 
-    pickle.dump(all_results, open("fssh.pickle", "wb"))
+    if (len(all_results) > 0):
+        pickle.dump(all_results, open(args.outfile, "wb"))
