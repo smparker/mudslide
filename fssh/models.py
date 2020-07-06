a856b5559df087022820587eef335b8fd03cd7cc
Shane Parker <smparker@uci.edu>
2020-07-06 18:51:11 -0400

    strip GPL from files

--- fssh/models.py
+++ fssh/models.py
@@ -3,22 +3,6 @@
 ## @package models
 #  Implementations of the one-dimensional two-state models Tully demonstrated FSSH on in Tully, J.C. <I>J. Chem. Phys.</I> 1990 <B>93</B> 1061.
 
-# fssh: program to run surface hopping simulations for model problems
-# Copyright (C) 2018-2020, Shane Parker <shane.parker@case.edu>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import numpy as np
 from scipy.special import erf
 
