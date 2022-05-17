0595f92d627023eb867bd7daf82d3578269f302f
Srijana Bhandari <sxb1131@case.edu>
2022-05-17 10:18:17 -0400

    return velocity

--- mudslide/math.py
+++ mudslide/math.py
@@ -5,6 +5,7 @@ from __future__ import division
 
 import numpy as np
 from .typing import ArrayLike
+from .constants import boltzmann
 
 def poisson_prob_scale(x: ArrayLike):
     """Computes (1 - exp(-x))/x which is needed when scaling Poisson probabilities"""
@@ -12,3 +13,17 @@ def poisson_prob_scale(x: ArrayLike):
         return 1 - x/2 + x**2/6 - x**3/24
     else:
         return -np.expm1(-x)/x
+
+def boltzmann_velocities(mass, temperature, scale=True, seed=None): 
+    rng = np.random.default_rng(seed)
+    kt = boltzmann * temperature
+    sigma = np.sqrt(kt * mass)
+    p = rng.normal(0.0, sigma)
+    if scale:
+        avg_KE = 0.5 * np.dot(p**2, np.reciprocal(mass))/mass.size
+        kbT2 = 0.5 * kt
+        scal = np.sqrt(kbT2/avg_KE)
+        p *= scal
+        p = p / mass
+    return p
+
