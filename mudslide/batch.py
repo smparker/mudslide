c029a720984a9913ed01f50162a81b0b5ce5f22d
Srijana Bhandari <sxb1131@case.edu>
2022-05-17 09:32:02 -0400

    remove boltzmann_velocity function

--- mudslide/batch.py
+++ mudslide/batch.py
@@ -17,21 +17,6 @@ from .typing import ModelT, TrajGenT, ArrayLike
 
 logger = logging.getLogger("mudslide")
 
-
-def boltzmann_velocities(mass, temperature, scale=True, seed=None): 
-    rng = np.random.default_rng(seed)
-    kt = boltzmann * temperature
-    sigma = np.sqrt(kt * mass)
-    p = rng.normal(0.0, sigma)
-    if scale:
-        avg_KE = 0.5 * np.dot(p**2, np.reciprocal(mass))/mass.size
-        kbT2 = 0.5 * kt
-        scal = np.sqrt(kbT2/avg_KE)
-        p *= scal
-    return p
-
-
-
 class TrajGenConst(object):
     """Canned class whose call function acts as a generator for static initial conditions
 
