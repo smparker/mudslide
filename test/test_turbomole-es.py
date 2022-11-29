e901cecb91437dc14a2607e1ab710146bcd704bc
Srijana Bhandari <sxb1131@case.edu>
2022-11-29 10:46:47 -0500

    add test for spawnstack

--- test/test_turbomole-es.py
+++ test/test_turbomole-es.py
@@ -10,6 +10,11 @@ import sys
 from pathlib import Path
 import mudslide
 import yaml
+import queue
+from mudslide.even_sampling import SpawnStack
+from mudslide.even_sampling import EvenSamplingTrajectory
+from mudslide.tracer import InMemoryTrace, YAMLTrace, TraceManager
+from mudslide.batch import TrajGenConst, TrajGenNormal, BatchedTraj
 
 from mudslide.models import TMModel
 from mudslide.tracer import YAMLTrace
@@ -22,15 +27,14 @@ def clean_directory(dirname):
     if os.path.isdir(dirname):
         shutil.rmtree(dirname)
 
-
 @unittest.skipUnless(mudslide.turbomole_model.turbomole_is_installed(), "Turbomole must be installed")
 class TestTMModel(unittest.TestCase):
     """Test Suite for TMModel class"""
 
     def setUp(self):
-        self.refdir = os.path.join(_refdir, "tm-c2h4")
+        self.refdir = os.path.join(_refdir, "tm-es-c2h4")
 
-        self.rundir = os.path.join(_checkdir, "tm-c2h4")
+        self.rundir = os.path.join(_checkdir, "tm-es-c2h4")
 
         clean_directory(self.rundir)
         os.makedirs(self.rundir, exist_ok=True)
@@ -46,8 +50,7 @@ class TestTMModel(unittest.TestCase):
 
     def test_get_gs_ex_properties(self):
         """test for gs_ex_properties function"""
-        model = TMModel(states=[0, 1, 2, 3],  expert=True)
-        positions = model.X
+        tm_model = TMModel(states=[0, 1, 2, 3], expert=True) 
 
         # yapf: disable
         mom = [ 5.583286976987380000, -2.713959745507320000,  0.392059702162967000,
@@ -57,15 +60,40 @@ class TestTMModel(unittest.TestCase):
                 2.145175145340160000,  0.594918215579156000,  1.075977514428970000,
                -2.269965412856570000,  0.495551832268249000,  1.487150300486560000]
         # yapf: enable
+        positions = tm_model.X
+        mass = tm_model.mass
+        q = queue.Queue()
+        dt = 20
+        max_time=81
+        t0 = 1
+        sample_stack = SpawnStack.from_quadrature(nsamples=[2, 2, 2])
+        samples = 1
+        nprocs = 1
+        trace_type = YAMLTrace
+        trace_options = {}
+        electronic_integration = 'exp'
+        trace_options["location"] = ""
+        trace_options["base_name"] = "TMtrace"
+        every = 1
+        model=tm_model
+        traj_gen = TrajGenConst(positions, mom, 3, dt)
 
-        log = mudslide.YAMLTrace(base_name="TMtrace", location=self.rundir, log_pitch=8)
-        traj = mudslide.TrajectorySH(model, positions, mom, 3, tracer=log, dt=20, max_time=81, t0=1,
-                                     seed_sequence=57892)
-        results = traj.simulate()
-
-        main_log = results.main_log
-
-        assert main_log == "TMtrace-0.yaml"
+        fssh = mudslide.BatchedTraj(model,
+                       traj_gen,
+                       trajectory_type=EvenSamplingTrajectory,
+                       mom=mom,
+                       positions=positions,
+                       samples=samples,
+                       max_time = max_time,
+                       nprocs=nprocs,
+                       dt=dt,
+                       t0=t0,
+                       tracemanager=TraceManager(TraceType=trace_type, trace_kwargs=trace_options),
+                       trace_every=every,
+                       spawn_stack=sample_stack,
+                       electronic_integration=electronic_integration)
+        results = fssh.compute()
+        outcomes = results.outcomes
 
         refs = mudslide.load_log(os.path.join(self.refdir, "traj-0.yaml"))
 
@@ -75,22 +103,23 @@ class TestTMModel(unittest.TestCase):
         for t in ref_times:
             for s in states:
                 self.assertAlmostEqual(refs[t]["electronics"]["hamiltonian"][s][s],
-                                       results[t]["electronics"]["hamiltonian"][s][s],
+                                       results[0][t]["electronics"]["hamiltonian"][s][s],
                                        places=8)
+
                 np.testing.assert_almost_equal(refs[t]["electronics"]["force"][s],
-                                               results[t]["electronics"]["force"][s],
+                                               results[0][t]["electronics"]["force"][s],
                                                decimal=8)
 
         for t in ref_times:
-            np.testing.assert_almost_equal(refs[t]["density_matrix"], results[t]["density_matrix"], decimal=8)
-            np.testing.assert_almost_equal(refs[t]["position"], results[t]["position"], decimal=8)
-            np.testing.assert_almost_equal(refs[t]["momentum"], results[t]["momentum"], decimal=8)
+            np.testing.assert_almost_equal(refs[t]["density_matrix"], results[0][t]["density_matrix"], decimal=8)
+            np.testing.assert_almost_equal(refs[t]["position"], results[0][t]["position"], decimal=8)
+            np.testing.assert_almost_equal(refs[t]["momentum"], results[0][t]["momentum"], decimal=8)
 
         for t in ref_times:
             for s1 in states:
                 for s2 in range(s1, 3):
                     np.testing.assert_almost_equal(refs[t]["electronics"]["derivative_coupling"][s1][s2],
-                                                   results[t]["electronics"]["derivative_coupling"][s1][s2],
+                                                   results[0][t]["electronics"]["derivative_coupling"][s1][s2],
                                                    decimal=6)
 
     def tearDown(self):
