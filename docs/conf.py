38d09d98d643b3e926fb648ddcc25ef1b2b4976e
Shane Parker <shane.parker@case.edu>
2023-04-22 23:59:47 -0400

    refs for docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ master_doc = 'index'
 # ones.
 extensions = [ 'sphinx.ext.autodoc', 'sphinxcontrib.bibtex' ]
 
+bibtex_bibfiles = [ "refs.bib" ]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
