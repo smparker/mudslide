5eac34b79e47ead4413ec8c9fc63c97eb2cd6ddf
Shane Parker <shane.parker@case.edu>
2024-11-10 14:54:10 -0500

    functions to remove COM and angular momentum

--- mudslide/math.py
+++ mudslide/math.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """Math helper functions"""
 
-from __future__ import division
-
+import warnings
 import numpy as np
+
+from .util import remove_center_of_mass_motion, remove_angular_momentum
+
 from .typing import ArrayLike
 from .constants import boltzmann
-import warnings
 
 
 def poisson_prob_scale(x: ArrayLike):
@@ -19,15 +20,40 @@ def poisson_prob_scale(x: ArrayLike):
     return out
 
 
-def boltzmann_velocities(mass, temperature, scale=True, seed=None):
+def boltzmann_velocities(mass, temperature, remove_translation=True,
+                         scale=True, seed=None):
+    """Generates random velocities according to the Boltzmann distribution.
+
+    :param mass: array of masses
+    :param temperature: target temperature
+    :param remove_translation: remove center of mass translation from velocities
+    :param remove_rotation: remove center of mass rotation from velocities
+    :param scale: scale velocities to match the target temperature
+    :param seed: random seed
+    :return: array of velocities
+    """
     rng = np.random.default_rng(seed)
     kt = boltzmann * temperature
     sigma = np.sqrt(kt * mass)
     p = rng.normal(0.0, sigma)
+
     if scale:
         avg_KE = 0.5 * np.dot(p**2, np.reciprocal(mass)) / mass.size
         kbT2 = 0.5 * kt
         scal = np.sqrt(kbT2 / avg_KE)
         p *= scal
-    p = p / mass
-    return p
+
+    v = p / mass
+
+    if remove_translation:
+        v3 = v.reshape((-1, 3))
+        M = mass.reshape((-1, 3))[:,0]
+
+        v = remove_center_of_mass_motion(v3, M).flatten()
+
+    #if remove_rotation:
+    #    v3 = v.reshape((-1, 3))
+    #    M = mass.reshape((-1, 3))[:,0]
+    #    v = remove_angular_momentum(v3, M, np.zeros_like(v3)).flatten()
+
+    return v
--- mudslide/util.py
+++ mudslide/util.py
@@ -4,6 +4,7 @@
 import os
 import sys
 
+import numpy as np
 
 def find_unique_name(name: str, location="", always_enumerate: bool = False, ending: str = "") -> str:
     """
@@ -32,3 +33,51 @@ def is_string(x) -> bool:
     :returns: True if x is a string
     """
     return isinstance(x, str)
+
+def remove_center_of_mass_motion(velocities: np.ndarray, masses: np.ndarray) -> np.ndarray:
+    """
+    Remove the center of mass motion from
+    a set of coordinates and masses.
+
+    :param velocities: velocities (n_atoms, 3)
+    :param masses: masses (n_atoms)
+
+    :returns: coordinates with center of mass motion removed
+    """
+    com = np.sum(velocities * masses[:, np.newaxis], axis=0) / np.sum(masses)
+    return velocities - com
+
+def remove_angular_momentum(velocities: np.ndarray, masses: np.ndarray, coordinates: np.ndarray) -> np.ndarray:
+    """
+    Remove the angular momentum from a set of coordinates, velocities, and masses.
+
+    :param velocities: velocities (n_atoms, 3)
+    :param masses: masses (n_atoms)
+    :param coordinates: coordinates (n_atoms, 3)
+
+    :returns: velocities with angular momentum removed
+    """
+    natom = velocities.shape[0]
+    com = np.sum(velocities * masses[:, np.newaxis], axis=0) / np.sum(masses)
+    com_coord = coordinates - com
+
+    momentum = np.einsum('ai,a->ai', velocities, masses)
+
+    # angular momentum
+    angular_momentum = np.cross(com_coord, momentum).sum(axis=0)
+
+    # inertial tensor
+    inertia = np.zeros((3, 3))
+    for i in range(natom):
+        x, y, z = com_coord[i]
+        inertia += masses[i] * (np.eye(3) * (x**2 + y**2 + z**2) -
+                                np.outer(com_coord[i], com_coord[i]))
+
+    # calculate angular velocity
+    angular_velocity = np.linalg.solve(inertia, angular_momentum)
+
+    corrected_velocities = velocities.copy()
+    for i in range(natom):
+        corrected_velocities[i] -= np.cross(angular_velocity, com_coord[i])
+
+    return corrected_velocities
