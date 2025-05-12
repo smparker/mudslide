f5da984ec8efd3e13fd1d21e00c8e893d9ba9652
Shane Parker <shane.parker@case.edu>
2025-05-12 16:03:23 -0400

    specify options in boltzmann test

--- test/test_boltzmann_velocity.py
+++ test/test_boltzmann_velocity.py
@@ -19,7 +19,8 @@ class TrajectoryTest(unittest.TestCase):
                           1837.15264736,  1837.15264736, 1837.15264736,
                           1837.15264736,  1837.15264736, 1837.15264736])
         # yapf: enable
-        velocities = boltzmann_velocities(mass, temperature=300, seed=109)
+        velocities = boltzmann_velocities(mass, temperature=300, remove_translation=False,
+                                          scale=True, seed=109)
 
         # yapf: disable
         results = [ 2.56748743e-04,  3.44471310e-04,  2.46424017e-07,
@@ -28,6 +29,21 @@ class TrajectoryTest(unittest.TestCase):
                     3.22950419e-04,  2.51235241e-04, -9.35539925e-04,
                     7.00590108e-05, -6.98837331e-04, -1.31891980e-03,
                    -8.41937362e-05,  9.68859510e-04,  3.86297562e-04]
+        # yapf: enable
+
+        np.testing.assert_almost_equal(results, velocities, decimal=8)
+
+        # yapf: enable
+        velocities = boltzmann_velocities(mass, temperature=300, remove_translation=True,
+                                          scale=True, seed=109)
+
+        # yapf: disable
+        results = [ 3.11566567e-04,  8.12744703e-05,  2.45304540e-05,
+                   -3.34929704e-04, -3.19461226e-05,  1.44339278e-04,
+                   -1.95088307e-04, -3.19011085e-04, -2.15392891e-04,
+                    3.77768244e-04, -1.19615991e-05, -9.11255895e-04,
+                    1.24876835e-04, -9.62034171e-04, -1.29463577e-03,
+                   -2.93759117e-05, 7.05662670e-04,  4.10581592e-04]
         # yapf: enable
 
         np.testing.assert_almost_equal(results, velocities, decimal=8)
