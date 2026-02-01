fix ordering in gradients from turboparse
turboparse produces a (natoms, 3) ordering in all gradients
mudslide directly flattens this improved ordering (no F style reshape)

mudslide/models/turbomole_model.py
@@ -173,7 +173,7 @@ class TurboControl:
 
     def get_masses(self, symbols):
         """Get the masses of the atoms in the system"""
-        atomic_masses = np.array([masses[s] for s in symbols for i in range(3)], dtype=np.float64)
+        atomic_masses = np.array([masses[s] for s in symbols for _ in range(3)], dtype=np.float64)
         atomic_masses *= amu_to_au
         return atomic_masses
 
@@ -415,7 +415,7 @@ class TMModel(ElectronicModel_):
                 i = dct["bra_state"]
                 j = dct["ket_state"]
 
-                ddr = np.array(dct["d/dR"]).reshape(self.ndof, order="F")
+                ddr = np.array(dct["d/dR"]).flatten()
                 self._derivative_coupling[i, j, :] = ddr
                 self._derivative_coupling[j, i, :] = -(self._derivative_coupling[i, j, :])
                 self._derivative_couplings_available[i, j] = True

test/test_turboparse.py
@@ -74,7 +74,7 @@ class TestHNAC:
         """Test gradient values - should be all zeros for this case"""
         gradient = parsed['egrad']['gradient'][0]
         # From lines 1579-1581: all zeros
-        expected = [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]]
+        expected = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
         assert np.allclose(gradient['d/dR'], expected), \
             f"Expected d/dR={expected}, got {gradient['d/dR']}"
 
@@ -92,9 +92,8 @@ class TestHNAC:
         # dE/dy  0.2216329D+00  0.0000000D+00
         # dE/dz -0.1151528D+00  0.0000000D+00
         expected_nac = [
-            [-0.1251235, 0.0],
-            [0.2216329, 0.0],
-            [-0.1151528, 0.0]
+            [-0.1251235, 0.2216329, -0.1151528],
+            [0.0, 0.0, 0.0]
         ]
         assert 'd/dR' in coupling
         assert np.allclose(coupling['d/dR'], expected_nac)
@@ -172,7 +171,7 @@ class TestHHeS2S:
         # dE/dx  0.0000000D+00  0.0000000D+00
         # dE/dy  0.0000000D+00  0.0000000D+00
         # dE/dz  0.7039368D-01 -0.7039368D-01
-        expected = [[0.0, 0.0], [0.0, 0.0], [0.07039368, -0.07039368]]
+        expected = [[0.0, 0.0, 0.07039368], [0.0, 0.0, -0.07039368]]
         assert np.allclose(gradient['d/dR'], expected)
 
     def test_coupling_parsed(self, parsed):
@@ -192,9 +191,8 @@ class TestHHeS2S:
         assert coupling['bra_state'] == 1
         assert coupling['ket_state'] == 2
         expected_nac = [
-            [0.0, 0.0],  # dx
-            [0.0, 0.0],  # dy
-            [-0.06646843, 0.05750408]  # dz
+            [0.0, 0.0, -0.06646843],  # atom 1
+            [0.0, 0.0, 0.05750408]  # atom 2
         ]
         assert np.allclose(coupling['d/dR'], expected_nac)
 
@@ -208,9 +206,8 @@ class TestHHeS2S:
         assert coupling['bra_state'] == 0
         assert coupling['ket_state'] == 1
         expected_nac = [
-            [0.0, 0.0],  # dx
-            [0.0, 0.0],  # dy
-            [0.07739182, -0.2188796]  # dz
+            [0.0, 0.0, 0.07739182],  # atom 1
+            [0.0, 0.0, -0.2188796]  # atom 2
         ]
         assert np.allclose(coupling['d/dR'], expected_nac)
 
@@ -224,9 +221,8 @@ class TestHHeS2S:
         assert coupling['bra_state'] == 0
         assert coupling['ket_state'] == 2
         expected_nac = [
-            [0.0, 0.0],  # dx
-            [0.0, 0.0],  # dy
-            [0.009668936, 0.1534590]  # dz
+            [0.0, 0.0, 0.009668936],  # atom 1
+            [0.0, 0.0, 0.1534590]  # atom 2
         ]
         assert np.allclose(coupling['d/dR'], expected_nac)
 