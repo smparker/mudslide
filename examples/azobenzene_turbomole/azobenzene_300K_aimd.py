d1ec44c9ea01d955b41138a0d664ef666aeb7b07
Shane Parker <shane.parker@case.edu>
2026-02-01 23:00:20 -0500

    only run azobenzene as __main__

--- examples/azobenzene_turbomole/azobenzene_300K_aimd.py
+++ examples/azobenzene_turbomole/azobenzene_300K_aimd.py
@@ -1,21 +1,24 @@
 import mudslide
 import numpy as np
 
-model = mudslide.models.TMModel(states=[0])
+if __name__ == "__main__":
+    mudslide.print_header()
 
-X = model._position
-velocities = mudslide.math.boltzmann_velocities(model.mass, temperature=500.0, seed=1234)
-velocities = mudslide.util.remove_angular_momentum(velocities.reshape((-1,3)), model.mass.reshape((-1,3))[:,0], X.reshape((-1,3))).flatten()
+    model = mudslide.models.TMModel(states=[0])
 
-traj = mudslide.AdiabaticMD(model, X, velocities,
-                            propagator={ "type": "nhc", "temperature": 300},
-                            dt=0.5*mudslide.fs_to_au,
-                            max_steps=100,
-                            remove_com_every=1,
-                            remove_angular_momentum_every=1)
+    X = model._position
+    velocities = mudslide.math.boltzmann_velocities(model.mass, temperature=500.0, seed=1234)
+    velocities = mudslide.util.remove_angular_momentum(velocities.reshape((-1,3)), model.mass.reshape((-1,3))[:,0], X.reshape((-1,3))).flatten()
 
-results = traj.simulate()
+    traj = mudslide.AdiabaticMD(model, X, velocities,
+                                propagator={ "type": "nhc", "temperature": 300},
+                                dt=0.5*mudslide.fs_to_au,
+                                max_steps=100,
+                                remove_com_every=1,
+                                remove_angular_momentum_every=1)
 
-results.print()
+    results = traj.simulate()
 
-mudslide.io.write_trajectory_xyz(model, results, 'azobenzene_trajectory.xyz')
+    results.print()
+
+    mudslide.io.write_trajectory_xyz(model, results, 'azobenzene_trajectory.xyz')
